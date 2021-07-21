05d6b2e32bc4b15e556a6a42c906c9b5b47e9329
Dmytro Sagaidachnyi <lafuma4me@gmail.com>
2021-07-22 01:36:40 +0300

    correct

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ export default class App extends Component {
 
     state = {
         contacts: [
-            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}, 
+            // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+            // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+            // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+            // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}, 
         ],
         filter: ''
     };
@@ -27,8 +27,8 @@ export default class App extends Component {
        name,
        number
        };
-
-        if (this.state.contacts.find((item) => item.name.toLowerCase() === name.toLowerCase())) {
+  
+          if (this.state.contacts.find((item) => item.name.toLowerCase() === name.toLowerCase())) {
             alert(`${name} is already in contacts.`);
         } else {
             this.setState(prevState => {
@@ -37,6 +37,7 @@ export default class App extends Component {
                 };
             });
         };
+
     };
     
     removeContact = contactId => {
